cf2a9c8d187c194f6b04fb14f490eb5ff88219a4
55heyerjt01 <55heyerjt01@MUP-R-EH207L23.PASSHE.LCL>
2017-03-07 09:31:22 -0500

    Update C14N06

--- JavaFXprac/src/Week6/C14N06.java
+++ JavaFXprac/src/Week6/C14N06.java
@@ -6,12 +6,8 @@
 package Week6;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
